e4d9c141fb28245b4f25225eb82f12e497893805
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-01-10 19:58:28 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Benjamin Lavallee on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
